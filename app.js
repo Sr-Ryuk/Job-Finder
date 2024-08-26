43889b2bb722131da939a7ba305b27684dde5551
devDiogo <diogo.wallaceferreira@gmail.com>
2024-08-25 22:28:02 -0300

    Criando os models, routes iniciais e exportando para o app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const db = require('./db/connection');
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
+
+// body parser
+app.use(bodyParser.urlencoded({extended: false}));
 
 // 
 
@@ -24,3 +28,7 @@ db
 app.get('/', (req,res) => {
     res.send("Está funcionando !");
 });
+
+// Jobs routers
+
+app.use('/jobs', require('./routes/jobs'));
